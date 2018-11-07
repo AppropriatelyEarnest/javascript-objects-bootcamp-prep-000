441bfafa759ada72fc54a68d9a854276433b4c60
ruthnewman <ruthnewman@gmail.com>
2018-11-07 11:24:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@ var meals = new Object( { breakfast: 'oatmeal' });
 var playlist = new Object({ "Kacey Musgraves": "High Horse"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
 return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
